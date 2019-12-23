d8d48baf966f97fb587cdd66eef65c42e20aa395
lakshmiprasanna44 <nlakshmiprasanna44@gmail.com>
2019-12-23 15:21:57 +0530

    bug fixes

--- src/app/services/viewaccess.service.ts
+++ src/app/services/viewaccess.service.ts
@@ -1,28 +1,25 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { GroupMasterModel,AccessNameModel,AccessNameModelNew, AutrAuthorizationItemmodel,checkboxSelect } from '../Models/config.model';
-import { GroupMastergetallModel,RoleNameModel } from '../Models/config.model';
-import { GroupNameModel,AccessRoleModel} from '../Models/config.model';
+import { GroupMasterModel, AccessNameModel, AccessNameModelNew, AutrAuthorizationItemmodel, checkboxSelect } from '../Models/config.model';
+import { GroupMastergetallModel, RoleNameModel } from '../Models/config.model';
+import { GroupNameModel, AccessRoleModel } from '../Models/config.model';
 import { RoleAccessModel } from '../Models/config.model';
-
-
+import { constants } from '../Models/MPRConstants'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ViewAccessService {
-  //url = 'http://10.29.15.165:90/Api/MPR';
-  url = 'http://localhost:49659/Api/ConfigAccess';
-
+  constructor(private http: HttpClient, private constants: constants) { }
+  public url = this.constants.url;
   public httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
-  constructor(private http: HttpClient) { }
 
-  getAuthItemDataforView(roleid:any): Observable<any> {  
-    return this.http.get<any>(this.url + '/getAuthorizationItemDetailById' + '?roleid=' + roleid);  
+  getAuthItemDataforView(roleid: any): Observable<any> {
+    return this.http.get<any>(this.url + 'ConfigAccess/getAuthorizationItemDetailById' + '?roleid=' + roleid);
   }
-  getAllAccessNameData(): Observable<AccessNameModel[]>{
-    return this.http.get<AccessNameModel[]>(this.url + '/getAllAccessNameData');
+  getAllAccessNameData(): Observable<AccessNameModel[]> {
+    return this.http.get<AccessNameModel[]>(this.url + 'ConfigAccess/getAllAccessNameData');
   }
 }
